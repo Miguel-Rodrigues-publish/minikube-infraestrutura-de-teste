7063b3313dbb172755c6e9cc7bcbb35ff8375d14
Miguel Rodrigues <miguel.b.rodrigues@gmail.com>
2022-06-24 14:06:31 +0100

    divisao codigo terraform

--- iac/terraform/codigo_terraform/main.tf
+++ iac/terraform/codigo_terraform/main.tf
@@ -12,17 +12,3 @@ terraform {
     path = "../terraform_internos/terraform.tfstate"
   }
 }
-
-
-
-provider "kubernetes" {
-  config_context_cluster  = local.valores.nome_cluster           # qual o cluster que vais ser usado
-  config_path             = local.valores.configuracao_cluster   # ficheiro de configuração acesso cluster
-}
-
-resource "kubernetes_namespace" "meus_recursos_de_namespaces" {
-  for_each = toset(local.valores.meus_namespaces)
-  metadata {
-    name = each.key
-  }
-}
